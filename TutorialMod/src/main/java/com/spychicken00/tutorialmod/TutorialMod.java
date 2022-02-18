7b2e92c6db6eae41fc2c55c3201fe387a7e05591
SpyChicken00 <94644105+SpyChicken00@users.noreply.github.com>
2022-02-18 17:45:55 -0500

    Minecraft Tutorial Mod- Currently Does Nothing, only loads into game as a valid mod

---
+++ TutorialMod/src/main/java/com/spychicken00/tutorialmod/TutorialMod.java
@@ -0,0 +1,12 @@
+package com.spychicken00.tutorialmod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("tutorialmod")
+public class TutorialMod {
+	public TutorialMod() {
+		MinecraftForge.EVENT_BUS.register(this);
+		
+	}
+}
